a07f81ad8b0aa74622d66f597a8817f202053fb3
robcronin <croninro@tcd.ie>
2023-05-20 13:03:51 +0100

    Update day script from template

--- scripts/newDay.sh
+++ scripts/newDay.sh
@@ -3,13 +3,17 @@ mkdir src/day$1
 
 if [ $? -ne 0 ] ; then
   echo "Day already exists!"
+  code src/day$1/day$1.data.ts src/day$1/day$1.ts src/day$1/day$1.test.ts
+  yarn test day$1
   exit
 fi
 
-echo "export const day$1 = (input: number[]) => {
+echo "
+export const day$1 = (input: string[]) => {
   return $1;
 };
-export const day$1part2 = (input: number[]) => {
+
+export const day$1part2 = (input: string[]) => {
   return $1;
 };
 " >> src/day$1/day$1.ts
@@ -17,10 +21,7 @@ export const day$1part2 = (input: number[]) => {
 echo "import { logAnswer } from '../utils/logging';
 import { parseInput } from '../utils/input';
 import { day$1, day$1part2 } from './day$1';
-import { data } from './day$1.data';
-
-const testString = '';
-const testData = parseInput(testString);
+import { data, testData } from './day$1.data';
 
 describe.only('day $1', () => {
   it.only('test cases', () => {
@@ -31,7 +32,6 @@ describe.only('day $1', () => {
   it('answer', () => {
     const answer = day$1(data);
     logAnswer(answer, $1, 1);
-    expect(typeof answer).toBe('number');
     expect(answer).toBe($1);
   });
 });
@@ -44,17 +44,18 @@ describe('day $1 part 2', () => {
   it('answer', () => {
     const answer = day$1part2(data);
     logAnswer(answer, $1, 2);
-    expect(typeof answer).toBe('number');
     expect(answer).toBe($1);
   });
 });" >> src/day$1/day$1.test.ts
 
 echo "import { parseInput } from '../utils/input';
 
+const testString = '';
 const input = '';
 
-export const data = parseInput(input) as number[];" >> src/day$1/day$1.data.ts
+export const testData = parseInput(testString) as string[];
+export const data = parseInput(input) as string[];" >> src/day$1/day$1.data.ts
 
-code src/day$1/day$1.ts src/day$1/day$1.data.ts src/day$1/day$1.test.ts
+code src/day$1/day$1.data.ts src/day$1/day$1.ts src/day$1/day$1.test.ts
 yarn test day$1
 exit
